009450ed663cfa3c6b3e049f6d2bb8c5032684c1
Jack Goode <jack.w.goode@icloud.com>
2020-06-05 00:56:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-   if array.length == 1
-      array.join
-   else if array.length == 2
+  if array.length > 2
+   last_el = array.pop()
+   array.push("and #{last_el}")
+   array.join(", ")
+ elsif array.length == 2
       array.join(" and ")
-    else
-     array.insert(-1, "and #{array[-1]}")
-      array.join(", ")
-    end  
+ else  array.join()
+
+  end
 end
